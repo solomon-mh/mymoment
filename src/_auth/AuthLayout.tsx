1f0d63556e99b07f61ee89df6a0d5a6a3b6048ce
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-01 08:51:03 +0300

    AuthLayout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,25 @@
+import { Outlet, Navigate } from "react-router-dom";
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/' />
+      ) : (
+        <>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
+            <Outlet />
+          </section>
+          <img
+            src='/assets/images/side-img.svg'
+            alt='logo'
+            className='hidden lg:block h-screen w-1/2 object-cover bg-no-repeat'
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
