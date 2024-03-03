1589318dc6454134929e55076d74372eeea32d42
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-03 19:44:41 +0300

    adding signup

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
         {/* public routes */}
         <Route element={<AuthLayout />}>
           <Route path='/sign-up' element={<SignupForm />} />
+          <Route path='/sign-in' element={<SignupForm />} />
         </Route>
         {/* private routes */}
         <Route element={<RootLayout />}>
