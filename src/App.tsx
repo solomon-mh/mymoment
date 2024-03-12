346fb2aadc7c3de8084d18e88e726075638db5c0
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-03-12 12:55:46 +0300

    adding new pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import AuthLayout from "./_auth/AuthLayout";
 import SignupForm from "./_auth/forms/SignupForm";
 import SigninForm from "./_auth/forms/SigninForm";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  Home,
+  Explore,
+  PostDetails,
+  EditPost,
+  CreatePost,
+  Saved,
+} from "./_root/pages";
 import { Toaster } from "@/components/ui/toaster";
 
 const App = () => {
@@ -19,6 +27,13 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<EditPost />} />
+          <Route path='/post/:id' element={<PostDetails />} />
+          <Route path='/explore' element={<Explore />} />
         </Route>
       </Routes>
       <Toaster />
